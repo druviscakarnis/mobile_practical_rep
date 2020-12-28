8ba9f9018956b5591dde3cd80f57d13aadbc468d
druviscakarnis <druvis.cakarnis@gmail.com>
2020-12-28 17:05:48 +0200

    V1.2
    Practical work 3

--- app/src/main/java/com/group/hello_group_1_pract/Activity2nd.java
+++ app/src/main/java/com/group/hello_group_1_pract/Activity2nd.java
@@ -2,12 +2,24 @@ package com.group.hello_group_1_pract;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+<<<<<<< Updated upstream
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class Activity2nd extends AppCompatActivity {
 
+=======
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Activity2nd extends AppCompatActivity {
+    private final String SHARED_PREFERENCES = "prefs";
+>>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +27,26 @@ public class Activity2nd extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("2-nd Activity");
+<<<<<<< Updated upstream
+=======
+
+        Button read = findViewById(R.id.load);
+        TextView textView = findViewById(R.id.prefs_text);
+
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences prefs = getSharedPreferences(SHARED_PREFERENCES,MODE_PRIVATE);
+
+                String text = prefs.getString("Input", "");
+                if(!text.equals("") && text!=null && !text.equals(" ")){
+                    textView.setText(text);
+                }else{
+                    Toast.makeText(Activity2nd.this, "Nothing found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+>>>>>>> Stashed changes
     }
     @Override
 
--- app/src/main/java/com/group/hello_group_1_pract/MainActivity.java
+++ app/src/main/java/com/group/hello_group_1_pract/MainActivity.java
@@ -1,5 +1,6 @@
 package com.group.hello_group_1_pract;
 
+<<<<<<< Updated upstream
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,10 +13,28 @@ import android.widget.Toast;
 
 import com.google.android.material.badge.BadgeUtils;
 
+=======
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+>>>>>>> Stashed changes
 public class MainActivity extends AppCompatActivity {
 
     String selectedCheckBox;
     String temp;
+<<<<<<< Updated upstream
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +44,95 @@ public class MainActivity extends AppCompatActivity {
         Button go2nd = findViewById(R.id.go_to_2nd);
         Button dialog = findViewById(R.id.go_dialog);
 
+=======
+    private Spinner spinner;
+    private final String SHARED_PREFERENCES = "prefs";
+
+    public String readPrefs(){
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFERENCES,MODE_PRIVATE);
+
+        String returns = prefs.getString("Input", "");
+
+        return returns;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Button go2nd = findViewById(R.id.go_to_2nd);
+        //Button dialog = findViewById(R.id.go_dialog);
+        Button save = findViewById(R.id.load);
+        EditText input = findViewById(R.id.input);
+        String text = readPrefs();
+        if(!text.equals("") && text!=null && !text.equals(" ")){
+            input.setText(text);
+        }
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* SAVE in preferences */
+                SharedPreferences prefs = getSharedPreferences(SHARED_PREFERENCES,MODE_PRIVATE);
+                SharedPreferences.Editor edit = prefs.edit();
+                edit.putString("Input", input.getText().toString());
+                edit.apply();
+
+                Log.v("Tests: ",input.getText().toString());
+                String text = prefs.getString("Input", "");
+                Log.v("Shared: ", text);
+            }
+        });
+>>>>>>> Stashed changes
         go2nd.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 Intent go_to_2nd = new Intent(getApplicationContext(), Activity2nd.class);
                 startActivity(go_to_2nd);
             }
+<<<<<<< Updated upstream
         });
 
 
         dialog.setOnClickListener(new View.OnClickListener(){
+=======
+
+
+        });
+
+        spinner = findViewById(R.id.theme_selector);
+
+        String[] themes = getResources().getStringArray(R.array.theme_select);
+        ArrayAdapter adapt = new ArrayAdapter(this,android.R.layout.simple_spinner_item, themes);
+        adapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapt);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(parent.getId() == R.id.theme_selector){
+                    String value = parent.getItemAtPosition(position).toString();
+                    if(value.toLowerCase().equals("dark")){
+                        getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    }else if(value.toLowerCase().equals("light")){
+                        getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    }else if(value.toLowerCase().equals("default")){
+                        getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        /* DIALOGS */
+        /*dialog.setOnClickListener(new View.OnClickListener(){
+>>>>>>> Stashed changes
             @Override
             public void onClick(View v) {
                 String[] groupMembers = getResources().getStringArray(R.array.group_members_dialog);
@@ -75,6 +173,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+<<<<<<< Updated upstream
 
 
             /*public void onClick(View v) {
@@ -111,6 +210,11 @@ public class MainActivity extends AppCompatActivity {
                             alertDialog.show();
                         }*/
 
+=======
+*/
+
+
+>>>>>>> Stashed changes
     }
 }
 
